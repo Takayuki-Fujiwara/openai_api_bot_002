e5a1346916748974cda2f4d33e2e64245c762f0f
Takayuki Fujiwara <69888801+Takayuki-Fujiwara@users.noreply.github.com>
2023-12-22 16:09:28 +0900

    Update app.py
    直前の変更の取り消し

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def communicate():
         messages=messages
     )  
 
-    bot_message = response["choices"][0]["message"+"京都弁で答えてください。" ]
+    bot_message = response["choices"][0]["message"]
     messages.append(bot_message)
 
     st.session_state["user_input"] = ""  # 入力欄を消去
